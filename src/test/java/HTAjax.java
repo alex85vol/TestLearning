6f8c3295aa8b6fefa5da44c874fe35d1b4a4b706
alex85vol <alex5vol@gmail.com>
2016-10-24 01:14:34 +0300

    finished with home task

--- src/test/java/HTAjax.java
+++ src/test/java/HTAjax.java
@@ -27,17 +27,16 @@ public class HTAjax {
     String[] textAfter;
 
 
-
     @BeforeSuite
-    public  void warmUp(){
+    public void warmUp() {
         mainPage = "http://www.w3schools.com/xml/ajax_intro.asp";
         driver = new FirefoxDriver();
-        wait = new WebDriverWait(driver,10);
+        wait = new WebDriverWait(driver, 10);
         driver.navigate().to(mainPage);
     }
 
     @Test
-    public void verifyOpenPage(){
+    public void verifyOpenPage() {
         logoXPath = "//a[@class='w3schools-logo']";
         labelXPath = "//div[@class='w3-example']/h3";
         WebElement logo = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(logoXPath)));
@@ -48,12 +47,22 @@ public class HTAjax {
     }
 
     @Test
-    public void verifyTextChanges(){
-        int i,j;
+    public void verifyTextChanges() {
+        int i;
         textBefore = "Let AJAX change this text";
         textAfter = new String[]{"AJAX is not a programming language.", "AJAX is a technique for accesing web servers from a web page.", "AJAX stands for Asyncronous JavaScript And XML."};
-        textBeforeXPath ="//div[@id='demo']/h2" ;
-        textAfterXPath = new String[] {"//div[@id='demo']/p[1]","//div[@id='demo']/p[2]", "//div[@id='demo']/p[3]" };
+        textBeforeXPath = "//div[@id='demo']/h2";
+        textAfterXPath = new String[]{"//div[@id='demo']/p[1]", "//div[@id='demo']/p[2]", "//div[@id='demo']/p[3]"};
+        buttonXPath = "//div[@id='demo']/button";
+        WebElement textBeforeChanges = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(textBeforeXPath)));
+        Assert.assertEquals(textBefore, textBeforeChanges.getText());
+        WebElement button = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(buttonXPath)));
+        button.click();
+        for (i = 0; i < 3; i++) {
+            WebElement textAfterChanges = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(textAfterXPath[i])));
+            Assert.assertEquals(textAfter[i], textAfterChanges.getText());
+        }
+
 
     }
 
